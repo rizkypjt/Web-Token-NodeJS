4d32bd611a0a3debbeadf197e3118b2f6c97deec
rizkypjt <rizkyhidayatpanjaitan97@gmail.com>
2020-01-05 19:20:09 +0700

    setting up database mongoose, models and then create routes

--- server.js
+++ server.js
@@ -3,6 +3,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+mongoose.connect('mongodb:://localhost/apiusers', {
+    useMongoClient: true
+})
+
 var jwt = require('jsonwebtoken');
 var app = express();
 var router = express.Router();
@@ -20,5 +25,18 @@ app.use(bodyParser.json());
 mongoose.connect(config.database);
 app.set('secretKey', config.secret);
 app.use(cors());
+
+router.get('/', function(req, res){
+    res.send('ini di route Home!');
+})
+
+router.get('/users', function(req, res){
+    User.find({}, function(err, users){
+        res.json(users);
+    });
+});
+
+//prefix /api
+app.use('/api', router);
 
 app.listen(4000);
